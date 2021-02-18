d790ceff46b5b086d188779305c3b7b76dd0df1e
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-02-18 08:35:34 -0500

    new project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace calling_all_computers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
